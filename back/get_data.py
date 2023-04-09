b51ac5d8a861c79101f6eb42c213033629f49fbe
mommom111 <20l1165@andrew.ac.jp>
2023-04-09 23:42:51 +0900

    Add POST endpoint to insert data received via axios into SQLite database

--- back/get_data.py
+++ back/get_data.py
@@ -1,31 +1,23 @@
-# from flask import Flask, request
-# import sqlite3
-
-# app = Flask(__name__)
-
-# @app.route('/api/users', methods=['POST'])
-# def add_user():
-#     data = request.get_json()
-#     if data and 'ID' in data and 'name' in data:
-#         conn = sqlite3.connect('database.db')
-#         c = conn.cursor()
-#         c.execute("INSERT INTO users (ID, name) VALUES (?, ?)", (data['ID'], data['name']))
-#         conn.commit()
-#         conn.close()
-#         return "User added successfully"
-#     else:
-#         return "Invalid data"
-
-# if __name__ == '__main__':
-#     app.run()
-
-from flask import Flask
+from flask import Flask, request
+import sqlite3
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/api/users')
-def get_users():
-    return 'Users'
+@app.route('/api/users', methods=['POST'])
+def add_user():
+    data = request.get_json()
+    if data and 'ID' in data and 'name' in data:
+        conn = sqlite3.connect('database.db')
+        c = conn.cursor()
+        c.execute("INSERT INTO users (ID, name) VALUES (?, ?)", (data['ID'], data['name']))
+        conn.commit()
+        conn.close()
+        print(data)
+        return "User added successfully"
+    else:
+        return "Invalid data"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()

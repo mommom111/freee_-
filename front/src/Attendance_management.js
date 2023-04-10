344928872f0b91b840abf63cd6e689b39756c0a5
mommom111 <20l1165@andrew.ac.jp>
2023-04-10 17:38:03 +0900

    Implement program to display data received from Flask on frontend mount

--- front/src/Attendance_management.js
+++ front/src/Attendance_management.js
@@ -1,38 +1,38 @@
-import React from 'react'
+import React, { Component } from 'react';
+import axios from 'axios';
 
-function attendance_management() {
-  return (
-    <div>
-      <table border='1'>
-        <tbody>
-          <tr>
-            <th>ID</th>
-            <th>氏名</th>
-            <th>勤務予定</th>
-            <th>休憩</th>
-            <th>残業</th>
-            <th>労働時間</th>
-          </tr>
-          <tr>
-            <td>1</td>
-            <td>山田</td>
-            <td>6:00 ~ 11:00</td>
-            <td>-</td>
-            <td>-</td>
-            <td>5:00</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>川田</td>
-            <td>11:00 ~ 15:00</td>
-            <td>-</td>
-            <td>-</td>
-            <td>3:00</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  )
+class Attendance_management extends Component {
+  state = {
+    users: []
+  }
+
+  // マウント時、データ取得
+  componentDidMount() {
+    axios.get('http://127.0.0.1:5000/api/users')
+      .then(response => {
+        const users = response.data;
+        this.setState({ users });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  render() {
+    const { users } = this.state;
+
+    return (
+      <div>
+        <h1>Employee List</h1>
+        <ul>
+          {/* 取得したデータの表示 */}
+          {users.map(user => (
+            <li key={user.id}>{user.name}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
-export default attendance_management
+export default Attendance_management;

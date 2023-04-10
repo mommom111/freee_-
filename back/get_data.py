5e6dc992e50d9f55888701391d7a604124665891
mommom111 <20l1165@andrew.ac.jp>
2023-04-10 17:34:32 +0900

    Add endpoint to get users data and send it to frontend

--- back/get_data.py
+++ back/get_data.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 import sqlite3
 from flask_cors import CORS
+from flask import jsonify
 
 app = Flask(__name__)
 CORS(app)
@@ -18,6 +19,22 @@ def add_user():
         return "User added successfully"
     else:
         return "Invalid data"
+    
+@app.route('/api/users', methods=['GET'])
+def get_users():
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+    c.execute("SELECT * FROM users")
+    rows = c.fetchall()
+    users = []
+    for row in rows:
+        user = {
+            "id": row[0],
+            "name": row[1]
+        }
+        users.append(user)
+    conn.close()
+    return jsonify(users)
 
 if __name__ == '__main__':
     app.run()

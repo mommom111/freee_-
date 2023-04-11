01990f5b4e6ad074ef8997ecb9fc6cce2dca48b7
mommom111 <20l1165@andrew.ac.jp>
2023-04-11 15:55:33 +0900

    Implement error message handling based on submitted form data

--- back/get_data.py
+++ back/get_data.py
@@ -6,19 +6,36 @@ from flask import jsonify
 app = Flask(__name__)
 CORS(app)
 
+# @app.route('/api/users', methods=['POST'])
+# def add_user():
+#     data = request.get_json()
+#     if data and 'ID' in data and 'name' in data:
+#         conn = sqlite3.connect('database.db')
+#         c = conn.cursor()
+#         c.execute("INSERT INTO users (ID, name) VALUES (?, ?)", (data['ID'], data['name']))
+#         conn.commit()
+#         conn.close()
+#         print(data)
+#         return "User added successfully"
+#     else:
+#         return "Invalid data"
+
 @app.route('/api/users', methods=['POST'])
 def add_user():
     data = request.get_json()
-    if data and 'ID' in data and 'name' in data:
-        conn = sqlite3.connect('database.db')
+    if data is None or 'ID' not in data and 'name' not in data: # **IDの入力だけして設定ボタンを押すとそのままデータが挿入される。** あるいはバリデーションの使用
+        return jsonify({'message': 'Invalid request data!'}), 400
+    with sqlite3.connect('database.db') as conn:
         c = conn.cursor()
+        # Check if user already exists
+        c.execute("SELECT * FROM users WHERE ID=?", (data['ID'],))
+        existing_user = c.fetchone()
+        if existing_user:
+            return jsonify({'message': 'User with that ID already exists!'}), 409
+        # Add user to database
         c.execute("INSERT INTO users (ID, name) VALUES (?, ?)", (data['ID'], data['name']))
         conn.commit()
-        conn.close()
-        print(data)
-        return "User added successfully"
-    else:
-        return "Invalid data"
+    return jsonify({'message': 'User added successfully!'}), 201
     
 @app.route('/api/users', methods=['GET'])
 def get_users():

7d2eda4c1053d4e12f4a7281e9c1479e5a1d0228
mommom111 <20l1165@andrew.ac.jp>
2023-05-07 11:09:58 +0900

    feat:勤怠状況を確認するためにAttendance_management.jsでバックエンドとの通信処理を追加

--- front/src/Attendance_management.js
+++ front/src/Attendance_management.js
@@ -1,15 +1,47 @@
-import React, { Component } from 'react';
+import axios from 'axios';
+import React, { Component, useEffect } from 'react';
 
-class Attendance_management extends Component {
+const Attendance_management = () => {
 
-  render() {
+  const [employeeSalary, setEmployeeSalary] = useState([]);
 
-    return (
-      <div className='Attendance_management'>
-        従業員のリストと給与を表示するページ
-      </div>
-    );
-  }
+  useEffect(() => {
+    axios.get('https://freee-backend.tunnelto.dev/api/salary')
+    .then(response => {
+      const employeeSalary = response.data;
+      setEmployeeSalary(employeeSalary);
+      console.log(employeeSalary);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  }, []);
+
+  return (
+    <div className='Attendance_management'>
+      <h1>勤怠状況と給与ページ</h1>
+      <table>
+      <thead>
+        <tr>
+          <th>従業員ID</th>
+          <th>氏名</th>
+          <th>今月の勤務時間</th>
+          <th>今月の給与</th>
+        </tr>
+      </thead>
+      <tbody>
+        {employeeSalary.map(salary => (
+          <tr key={salary.id}>
+            <td>{salary.employee_id}</td>
+            <td>{salary.name}</td>
+            <td>{salary.shift_time}</td>
+            <td>{salary.salary}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+    </div>
+  );
 }
 
 export default Attendance_management;

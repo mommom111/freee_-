980786f9a0dfb9cf9c24145c39c268c2dc6132c8
mommom111 <20l1165@andrew.ac.jp>
2023-05-08 10:06:52 +0900

    add:時給計算を行うためにデータベースに時給を追加

--- back/mydb.py
+++ back/mydb.py
@@ -14,24 +14,33 @@ cursor.execute("""
 """)
 
 cursor.execute("""
+  ALTER TABLE employees
+  ADD COLUMN hourly_wage INTEGER DEFAULT 1000;
+""")
+
+cursor.execute("""
+  UPDATE employees
+  SET hourly_wage = 1000;
+""")
+
+cursor.execute("""
   CREATE TABLE IF NOT EXISTS shifts (
     id INTEGER PRIMARY KEY,
     employee_id INTEGER NOT NULL,
     shift_date DATE NOT NULL,
     shift_time TEXT CHECK(shift_time IN ('morning', 'night')) NOT NULL,
     user_id TEXT,
+    checked_in_time TIMESTAMP,
+    leaving_time TIMESTAMP,
+    working_hours REAL,
     FOREIGN KEY (employee_id) REFERENCES employees (id)
     FOREIGN KEY (user_id) REFERENCES LINE_users (user_id)
   );
 """)
 
 cursor.execute("""
-  ALTER TABLE shifts ADD COLUMN working_hours REAL;
-""")
-
-cursor.execute("""
-  UPDATE shifts SET working_hours = strftime('%s', leaving_time) - strftime('%s', checked_in_time);
-""")
+  UPDATE shifts SET working_hours = (strftime('%s', leaving_time) - strftime('%s', checked_in_time)) / 3600.0;
+""") 
 
 cursor.execute("""
   CREATE TABLE IF NOT EXISTS LINE_users (
